616290e127a7adc3c8965ed6000758e988d6fbc7
faith <3270363539@qq.com>
2025-04-13 10:09:53 +0800

    添加get_annotation,save_annotation对应的url

--- Epp-BackEnd/backend/backend/urls.py
+++ Epp-BackEnd/backend/backend/urls.py
@@ -47,6 +47,7 @@ urlpatterns = [
                   path("api/likeComment", paper_details.like_comment),
                   path("api/getUserPaperInfo", paper_details.get_user_paper_info),
                   path("api/saveAnnotation", paper_details.save_annotation),
+                  path("api/getAnnotations", paper_details.get_annotation),
 
                   # 用户上传论文模块
                   path("api/uploadPaper", upload_document.upload_paper),
