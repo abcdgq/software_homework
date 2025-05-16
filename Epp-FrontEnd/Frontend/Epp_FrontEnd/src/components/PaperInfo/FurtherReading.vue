bbe95ba816688de9bcce1c6bda8b353c091df0b4
zcl <zcl@buaa.edu.cn>
2025-05-16 10:21:51 +0800

    优化延伸阅读部分内容显示效果

--- Epp-FrontEnd/Frontend/Epp_FrontEnd/src/components/PaperInfo/FurtherReading.vue
+++ Epp-FrontEnd/Frontend/Epp_FrontEnd/src/components/PaperInfo/FurtherReading.vue
@@ -1,40 +1,50 @@
 <template>
     <el-card shadow="always" class="further-reading">
         <div class="header">
-            <span><el-icon>  </el-icon>延伸阅读</span>
-            <el-button icon="el-icon-arrow-down" @click="toggleCollapse"/>
+            <div class="header-left">
+                <span class="header-title" style="margin-right: 5px;">延伸阅读</span>
+                <el-icon class="el-icon-search" />
+            </div>
+            <el-button
+                :icon="collapsed? 'el-icon-arrow-down' : 'el-icon-arrow-up'"
+                @click="toggleCollapse"
+                circle plain
+            />
         </div>
 
         <el-collapse-transition>
             <div v-show="!collapsed">
                 <el-empty v-if="papers.length === 0 && !loading" description="暂无推荐文献"/>
                 <div v-else>
-                    <div
-                        v-for="(paper, index) in papers"
-                        :key="index"
-                        class="paper-card"
-                    >
-                        <div class="title"> {{ paper.title }}</div>
-                        <div class="summary"> {{ paper.summary }}</div>
+                    <el-row gutter="60">
+                        <el-col
+                            v-for="(paper, index) in papers"
+                            :key="index"
+                            :span="12"
+                            class="paper-card"
+                        >
+                            <div class="title">{{ paper.title }}🔥</div>
+                            <div class="summary">📌{{ paper.summary }}</div>
 
-                        <el-row class="card-actions">
-                            <router-link
-                                :to="{ name: 'paper-info', params: { paper_id: paper.id } }"
-                                style="margin-right: 12px;"
+                            <el-row class="card-actions">
+                                <router-link
+                                    :to="{ name: 'paper-info', params: { paper_id: paper.id } }"
+                                    style="margin-right: 12px;"
                                 >
+                                    <el-button
+                                        type="text"
+                                        icon="el-icon-document"
+                                    >查看论文</el-button>
+                                </router-link>
+
                                 <el-button
                                     type="text"
-                                    icon="el-icon-document"
-                                >查看论文</el-button>
-                            </router-link>
-
-                            <el-button
-                                type="text"
-                                :icon="paper.collected? 'el-icon-star-on' : 'el-icon-star-off'"
-                                @click="toggleCollected(index)"
-                            >收藏</el-button>
-                        </el-row>
-                    </div>
+                                    :icon="paper.collected? 'el-icon-star-on' : 'el-icon-star-off'"
+                                    @click="toggleCollected(index)"
+                                >收藏</el-button>
+                            </el-row>
+                        </el-col>
+                    </el-row>
                 </div>
             </div>
         </el-collapse-transition>
@@ -54,13 +64,13 @@ export default {
             papers: [
                 {
                     id: '',
-                    title: 'This is the title of the paper',
+                    title: 'Semantic-Guided Zero-Shot Learning for Low-Light Image/Video Enhancement',
                     summary: '一句话推荐',
                     collected: false // 该论文是否已经添加到收藏
                 },
                 {
                     id: '',
-                    title: 'This is the title of the paper',
+                    title: 'Semantic-Guided Zero-Shot Learning for Low-Light Image/Video Enhancement',
                     summary: '一句话推荐',
                     collected: false // 该论文是否已经添加到收藏
                 }
@@ -111,13 +121,31 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    font-weight: bold;
+    padding: 12px 8px;
+    border-bottom: 1px solid #ebeef5;
+    background-color: #f9f9f9;
+    border-radius: 8px;
     margin-bottom: 12px;
+}
+.header-left {
+    display: flex;
+    align-items: center;
+}
+.el-icon-search {
+    color: #409eff;
+    font-size: 20px;
+    margin-right: 8px;
+}
+.header-title {
+    font-size: 18px;
+    font-weight: 600;
+    color: #333;
 }
 
 .paper-card {
     margin-bottom: 12px;
     padding: 10px;
+    text-align: left;
 }
 
 .title {
